b4844459c89452e6adfa9d54d44c4e2cd029a019
Krisztian Sala <krisztian.sala@globant.com>
2022-07-29 16:25:55 +0300

    Working state

--- index.ts
+++ index.ts
@@ -12,19 +12,31 @@ interface ActionPermissions {
 }
 
 async function getTeamMembers(team: string): Promise<string[]> {
-    const teamMembers = await octokit.request('GET /orgs/{org}/teams/{teamSlug}/members', {
-        org: github.context.repo.owner,
-        teamSlug: team
-    })
-    return teamMembers.data.map((member: any) => member.login);
+    try {
+        // Sometimes teams in CODEOWNERS files are in the form of: organization/team-name
+        const teamSplit = team.split('/');
+        let owner = github.context.repo.owner;
+        if (teamSplit.length > 1) {
+            owner = teamSplit[0];
+            team = teamSplit[1];
+        }
+        const teamMembers = await octokit.request('GET /orgs/{org}/teams/{teamSlug}/members', {
+            org: owner,
+            teamSlug: team
+        })
+        return teamMembers.data.map((member: any) => member.login);
+    } catch (error) {
+        return [];
+    }
 }
 
-async function isUserAllowedByConfigFile(actor: string, configFile: string, workflow: string) {
+async function isUserAllowedByConfigFile(actor: string, configFile: string, workflow: string): Promise<boolean> {
     try {
         const file = await fs.readFile(configFile);
-        const actions = JSON.parse(file.toString()).action_permissions as ActionPermissions;
+        const actions: ActionPermissions = JSON.parse(file.toString()).action_permissions;
         if (!actions) {
-            core.error('Config file missing required property: action_permissions');
+            core.warning('Config file missing required property: action_permissions');
+            return false;
         }
 
         for (const [actionName, allowedActors] of Object.entries(actions)) {
@@ -32,37 +44,69 @@ async function isUserAllowedByConfigFile(actor: string, configFile: string, work
                 if (allowedActors.users && allowedActors.users.includes(actor)) {
                     return true;
                 }
-                
+
                 if (!allowedActors.teams) return false;
                 for (let team of allowedActors.teams) {
                     const members = await getTeamMembers(team);
-                    console.log(members);
-                    if(members.includes(actor)) return true;
+                    if (members.includes(actor)) return true;
                 }
                 return false;
             }
         }
-
-        // If the action is not in the config file, then the user is allowed
-        return true;
     } catch (e: any) {
-        core.error('Error occurred when loading config file ', e);
+        core.error(`Error occurred when loading config file ${e.toString()}`);
     }
     return false;
 }
 
+async function isUserAllowedByCodeowners(actor: string): Promise<boolean> {
+    try {
+        let file = null;
+        const codeownerPaths = ['CODEOWNERS', '.github/CODEOWNERS', 'docs/CODEOWNERS']
+        for (const codeownerPath of codeownerPaths) {
+            try {
+                file = await fs.readFile(codeownerPath);
+                break;
+            } catch (e) { }
+        }
+        if (!file) {
+            core.warning('CODEOWNERS not present in repository.');
+            return false;
+        }
+        // Not checking for the exact action file that is running, because the filename is not the context
+        const pathRegex = /^\*$|^\.github\/?\**$|^\.github\/workflows\/?\**$/g;
+        const lines = file.toString().split(/\r\n|\r|\n/);
+        for (const line of lines) {
+            if (!line) continue;
+            if (line.startsWith('#')) continue;
+            const [pathString, ...ownersRaw] = line.split(/\s+/);
+            const owners = ownersRaw.map(owner => owner.replace(/^\@/, ''));
+            if (pathString.match(pathRegex)) {
+                if (owners.includes(actor)) {
+                    return true;
+                }
+                for (const team of owners) {
+                    const members = await getTeamMembers(team);
+                    if (members.includes(actor)) return true;
+                }
+            }
+        }
+    } catch (e: any) {
+        core.warning(`Error occurred when parsing CODEOWNERS file ${e.toString()}`);
+    }
+    return false;
+}
 async function main() {
     const { actor, workflow } = github.context;
     const configFile = core.getInput('config_file', { required: false }) ?? '.devops.config';
     let allowed = await isUserAllowedByConfigFile(actor, configFile, workflow);
-    // allowed ||= await isUserAllowedByCodeowners(actor);
-    console.log(github);
+    allowed ||= await isUserAllowedByCodeowners(actor);
     if (allowed) {
         core.info("User allowed to run action");
         return;
     }
 
-    core.setFailed(`${actor} is not allowed to run this workflow`);
+    core.setFailed(`${actor} is not allowed to run this workflow. Add the user or their team to the config file or CODEOWNERS.`);
 }
 
 main();
